455de7d90191d1e75fa2c94259cf48f5ff3ad211
Remilekun-Elijah <38536025+Remilekun-Elijah@users.noreply.github.com>
2018-06-04 15:20:08 +0100

    all new folders

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-var chatFace = "";
-$(document).ready(function(){
-$("h1").html("<button class='btn btn-lg btn-primary'>Click Me</button>");
-$("body").css("padding-left","20px");
-$("button").addClass("animated bounce");
-
-});
 $(function(){
-alert( "Welcome Buddy");
-$("h1").click(function(){
-     var greetings  = ["I love fruit", "I like cake", "I prefer IceCream", "None is my thing", "It's really Cool!"];
-
-    $("input").attr(" type='button', value='click here'");
-    prompt("greetings");
-    
- 
-}); 
+    console.log("Main Init");
+    init();
+    console.log("Lets play the game nicely"); 
 });
+
+function init(){
+    console.log("init() called");
+};
